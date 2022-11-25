b5a881a749f724c325dcf921542741b94b0a763a
iljeee <plyusninilya97@gmail.com>
2022-11-25 13:20:45 +0100

    Added temp data

---
+++ src/CardData.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 1,
+        name: "Iron Man",
+        description: "Doubles Power at this location",
+        image: "https://tools.toywiz.com/_images/_webp/_products/lg/aug180958.webp",
+        stats: {
+            cost: 5,
+            power: 0
+        },
+        abilityType: "Ongoing",
+        rarity: 0
+    },
+    {
+        id: 2,
+        name: "War Machine",
+        description: "+5 Power if Iron Man is on this location",
+        image: "https://i0.wp.com/lasetadelgoblin.com/wp-content/uploads/2021/06/2-5.jpg?w=846&ssl=1",
+        stats: {
+            cost: 3,
+            power: 6
+        },
+        abilityType: "Ongoing",
+        rarity: 4
+    },
+    {
+        id: 3,
+        name: "Thor",
+        description: "Decides when this is over",
+        image: "https://i.pinimg.com/originals/df/f1/dd/dff1ddc600819e5f1aa8e95a788d5584.jpg",
+        stats: {
+            cost: 6,
+            power: 8
+        },
+        abilityType: "On Reveal",
+        rarity: 3
+    },
+    {
+        id: 4,
+        name: "Hulk",
+        description: "HULK SMASH",
+        image: "https://i.pinimg.com/originals/fe/52/fc/fe52fc80684e80b0b54ef012211ee61b.jpg",
+        stats: {
+            cost: 6,
+            power: 12
+        },
+        abilityType: "",
+        rarity: 4
+    },
+    {
+        id: 5,
+        name: "Black Widow",
+        description: "Adds Widow's Bite to opponent's hand",
+        image: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/11/10/550w_comics_fear_itself_black_widow.jpg?resize=480:*",
+        stats: {
+            cost: 2,
+            power: 2
+        },
+        abilityType: "On Reveal",
+        rarity: 1
+    },
+    {
+        id: 6,
+        name: "Captain America",
+        description: "Give all your cards on this location +1 Power",
+        image: "https://pm1.narvii.com/5937/1c271acc1a1028b2ed05299fe435c206f1f1de20_hq.jpg",
+        stats: {
+            cost: 3,
+            power: 6
+        },
+        abilityType: "Ongoing",
+        rarity: 4
+    },
+]
